9d8a959319f7a31fcc650e5eadfc4da8b8360607
timpostuvan <tim.postuvan@gmail.com>
2022-10-24 16:36:26 +0200

    Add requirements and installation script

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install sentence-transformers
+pip install torch==1.9.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+pip install torch-sparse==0.6.11 -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+pip install torch-geometric
+pip install -r requirements.txt
+python -m spacy download en
+pip install setuptools==59.5.0
