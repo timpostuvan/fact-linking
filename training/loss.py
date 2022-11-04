a82318635f71fbae835d6bdb507c266527eaeecb
timpostuvan <tim.postuvan@gmail.com>
2022-11-04 23:45:07 +0100

    Add Focal loss

--- training/loss.py
+++ training/loss.py
@@ -1,15 +1,53 @@
 from omegaconf import DictConfig
 from torch import nn
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FocalLoss(nn.Module):
+    """
+    Focal loss implemented as a PyTorch module.
+    [Original paper](https://arxiv.org/pdf/1708.02002.pdf).
+    """
+
+    def __init__(self, gamma: float, ignore_index: int, reduction='none'):
+        """
+        :param gamma: What value of Gamma to use. Value of 0 corresponds to Cross entropy.
+        :param reduction: Reduction to be done on top of datapoint-level losses.
+        """
+        super().__init__()
+
+        assert reduction in ['none', 'sum', 'mean']
+
+        self.gamma = gamma
+        self.reduction = reduction
+        self.ignore_index = ignore_index
+
+    def forward(self, input_logits: torch.Tensor, targets: torch.Tensor):
+        ce_loss = torch.nn.functional.cross_entropy(input_logits, targets, reduction='none', ignore_index=self.ignore_index)
+        input_probs_for_target = torch.exp(-ce_loss)
+        loss = (1 - input_probs_for_target) ** self.gamma * ce_loss
+
+        if self.reduction == 'sum':
+            loss = loss.sum()
+        elif self.reduction == 'mean':
+            loss = loss.mean()
+
+        return loss
+
 
 def get_loss(config: DictConfig, ignore_index: int = -100):
     if config.loss == 'cross_entropy':
         loss_func = nn.CrossEntropyLoss(reduction='mean', ignore_index=ignore_index)
+    elif config.loss == 'focal_loss':
+        loss_func = FocalLoss(gamma=2, reduction='mean', ignore_index=ignore_index)
     else:
         raise ValueError(f"Unknown loss {config.loss}")
 
     def compute_loss(logits, labels):
-        if config.loss == 'cross_entropy':
+        if config.loss in ['cross_entropy', 'focal_loss']:
              loss = loss_func(logits, labels)
         else:
             raise ValueError(f"Unknown loss {config.loss}")
