ec2e480c69f7f283414cc0e3dec63bf51f2451ba
timpostuvan <tim.postuvan@gmail.com>
2022-11-11 15:18:03 +0100

    Remove graph information

--- models/qagnn.py
+++ models/qagnn.py
@@ -1,4 +1,3 @@
-from platform import node
 from typing import Optional
 
 import torch
@@ -55,7 +54,7 @@ class QAGNN(nn.Module):
         self.pooler = MultiheadAttPoolLayer(n_attn_head, sent_dim, concept_dim)
 
         self.fc = MLP(
-            input_size=concept_dim + concept_dim,
+            input_size=sent_dim + sent_dim,
             hidden_size=fc_dim,
             output_size=2,
             num_layers=n_fc_layers,
@@ -88,46 +87,17 @@ class QAGNN(nn.Module):
         edge_index_ids,
         edge_type_ids,
     ):
-        
-        sent_vecs_projected = self.activation(self.sent_projection(sent_vecs)).unsqueeze(1)     # (batch_size, 1, dim_node)
-        
-        concept_vecs = self.concept_emb(concept_ids[:, 1:] - 1)     # (batch_size, n_node - 1, dim_node)
+        sent_vecs = sent_vecs.unsqueeze(1)     # (batch_size, 1, dim_sent)
+        initial_node_embeddings = torch.cat([sent_vecs, self.concept_emb(concept_ids[:, 1:] - 1)], dim=1)    # (batch_size, n_node, dim_sent)
 
-        gnn_input = self.dropout_e(torch.cat([sent_vecs_projected, concept_vecs], dim=1))   # (batch_size, n_node, dim_node)
-
-        gnn_output = self.gnn(
-            gnn_input,
-            edge_index_ids,
-            edge_type_ids,
-            node_type_ids,
-            node_scores
-        )
-
-        sent_vecs_after_gnn = gnn_output[:, 0]      # (batch_size, dim_node)
-
-        # 1 means masked out
-        mask = torch.arange(node_type_ids.size(1), device=node_type_ids.device) >= adj_lengths.unsqueeze(1)
-
-        # pool over all KG nodes, but not QAGNN_contextnode or padding nodes
-        mask = mask | (node_type_ids == 0) | (node_type_ids == 2)
-
-        # a temporary solution to avoid zero node
-        mask[mask.all(1), 0] = 0
-
-        graph_vecs, pool_attn = self.pooler(sent_vecs, gnn_output, mask)
-
-        node_embeddings = gnn_output     # (batch_size, n_node, dim_node)
-        num_nodes = node_embeddings.shape[1]
-        graph_vecs = graph_vecs.unsqueeze(1).repeat(1, num_nodes, 1)    # (batch_size, n_node, dim_node)
-        sent_vecs = sent_vecs.unsqueeze(1).repeat(1, num_nodes, 1)      # (batch_size, n_node, dim_sent)
-        sent_vecs_after_gnn = sent_vecs_after_gnn.unsqueeze(1).repeat(1, num_nodes, 1)  # (batch_size, n_node, dim_node)
+        num_nodes = initial_node_embeddings.shape[1]
 
         # when predicting relevance of a node, we consider: 
-        # node embedding and embedding of QAGNN_contextnode
-        concat = torch.cat([node_embeddings, sent_vecs_after_gnn], dim=-1)  
+        # static node embedding and context LM embedding
+        concat = torch.cat([initial_node_embeddings, sent_vecs.repeat(1, num_nodes, 1)], dim=-1)    # (batch_size, n_node, 2 * dim_sent)
         concat = self.dropout_fc(concat)
         logits = self.fc(concat).transpose(1, 2)    # (batch_size, 2, num_nodes)
-        return logits, pool_attn
+        return logits, -1
 
 
 class LM_QAGNN(nn.Module):
